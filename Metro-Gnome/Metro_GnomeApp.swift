69bef39e3d1ae5e78c06873e250c4e89cdc365b5
Zachary Eidenberger <zacharyg88@gmail.com>
2023-09-12 15:17:42 -0400

    Initial Commit

---
+++ Metro-Gnome/Metro_GnomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metro_GnomeApp.swift
+//  Metro-Gnome
+//
+//  Created by Zach Eidenberger on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Metro_GnomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
